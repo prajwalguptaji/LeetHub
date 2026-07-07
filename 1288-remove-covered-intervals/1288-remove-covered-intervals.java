8949784dc54ad08bbd1ebad7a56d45d6f70d7282
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-07 17:14:46 +0530

    Time: 9 ms (9.19%), Space: 46.5 MB (62.63%) - LeetHub

--- 1288-remove-covered-intervals/1288-remove-covered-intervals.java
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -1,24 +1,41 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-        int count=0;
-        int i=0,j=0,n=intervals.length;
+        // int count=0;
+        // int i=0,j=0,n=intervals.length;
         
-        for(i=0;i<n;i++){
-            boolean isCovered=false;
-            for(j=0;j<n;j++){
-                //(a,b) (c,d)  c>=a  &&   d<=b
-                if(i!=j && (intervals[j][0]<=intervals[i][0] && intervals[j][1]>=intervals[i][1])){
-                    isCovered=true;
-                    break;
-                }
+        // for(i=0;i<n;i++){
+        //     boolean isCovered=false;
+        //     for(j=0;j<n;j++){
+        //         //(a,b) (c,d)  c<=a  &&   d>=b
+        //         if(i!=j && (intervals[j][0]<=intervals[i][0] && intervals[j][1]>=intervals[i][1])){
+        //             isCovered=true;
+        //             break;
+        //         }
                
+        //     }
+        //     if(!isCovered){
+        //      count++;
+        //     }
+        // }
+         
+        // return count;
+        Arrays.sort(intervals,(a,b)->{
+            if(a[0]==b[0]){
+                return  Integer.compare(b[1],a[1]);
             }
-            if(!isCovered){
-             count++;
+            return Integer.compare(a[0],b[0]);
+            });
+        int count =1;
+        int maxEnd=intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][1]>maxEnd){
+                count++;
+                maxEnd=intervals[i][1];
             }
         }
-         
         return count;
+
+
         
     }
 }
