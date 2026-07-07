054ccc16fa12d8d60e62c0e9cbce1226670a98b3
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-08 00:09:03 +0530

    Time: 358 ms (9.76%), Space: 47.7 MB (38.27%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.java
+++ 0731-my-calendar-ii/0731-my-calendar-ii.java
@@ -1,34 +1,55 @@
-class MyCalendarTwo {
-     List<int[]> bookings;
-     List<int[]> overlaps; 
+// class MyCalendarTwo {
+//      List<int[]> bookings;
+//      List<int[]> overlaps; 
 
-    public MyCalendarTwo() {
-       bookings= new ArrayList<>();
-       overlaps= new ArrayList<>();
+//     public MyCalendarTwo() {
+//        bookings= new ArrayList<>();
+//        overlaps= new ArrayList<>();
 
 
-    }
+//     }
     
-    public boolean book(int start, int end) {
-        // check Triple Bookings
-        for(int[] interval: overlaps){
-            if(start<interval[1]&& end> interval[0]){
+//     public boolean book(int start, int end) {
+//         // check Triple Bookings
+//         for(int[] interval: overlaps){
+//             if(start<interval[1]&& end> interval[0]){
+//                 return false;
+//             }
+//         }
+//         //double booking
+//         for(int[] interval : bookings){
+//             if(start<interval[1]&&end>interval[0]){
+//                 overlaps.add(new int[]{
+//                  Math.max(start,interval[0]),
+//                  Math.min(end,interval[1])
+//                 });
+//             }
+
+//         }
+//         bookings.add(new int[]{start,end});
+//         return true;
+        
+//     }
+// }
+
+class MyCalendarTwo {
+    Map<Integer,Integer> map;
+    public MyCalendarTwo(){
+        map=new TreeMap<Integer,Integer>();
+    }
+    public boolean book(int startTime,int endTime){
+        map.put(startTime,map.getOrDefault(startTime,0)+1);
+        map.put(endTime,map.getOrDefault(endTime,0)-1);
+        int bookings=0;
+        for(Map.Entry<Integer,Integer>entry:map.entrySet()){
+            bookings=bookings+entry.getValue();
+            if(bookings > 2){
+                map.put(startTime,map.get(startTime)-1);
+                map.put(endTime,map.get(endTime)+1);
                 return false;
             }
         }
-        //double booking
-        for(int[] interval : bookings){
-            if(start<interval[1]&&end>interval[0]){
-                overlaps.add(new int[]{
-                 Math.max(start,interval[0]),
-                 Math.min(end,interval[1])
-                });
-            }
-
-        }
-        bookings.add(new int[]{start,end});
         return true;
-        
     }
 }
 
