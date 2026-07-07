74f4b3e1a6732897a673a9f78d64f46f1868eaf9
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-07 16:09:14 +0530

    Time: 4 ms (97.24%), Space: 46.7 MB (24.35%) - LeetHub

---
+++ 1288-remove-covered-intervals/1288-remove-covered-intervals.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int removeCoveredIntervals(int[][] intervals) {
+        int count=0;
+        int i=0,j=0,n=intervals.length;
+        
+        for(i=0;i<n;i++){
+            boolean isCovered=false;
+            for(j=0;j<n;j++){
+                //(a,b) (c,d)  c>=a  &&   d<=b
+                if(i!=j && (intervals[j][0]<=intervals[i][0] && intervals[j][1]>=intervals[i][1])){
+                    isCovered=true;
+                    break;
+                }
+               
+            }
+            if(!isCovered){
+             count++;
+            }
+        }
+         
+        return count;
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
