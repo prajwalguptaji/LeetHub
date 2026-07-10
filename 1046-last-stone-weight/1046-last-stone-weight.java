47331d600d98b3fa33082524efd2d3efee19c1e5
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-10 22:52:13 +0530

    Time: 1 ms (99.49%), Space: 42.5 MB (93.2%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,21 +1,40 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        List<Integer> list=new ArrayList<>();
-        for(int k:stones){
-            list.add(k);
-        }
-        while(list.size()>1){
-            Collections.sort(list);
-            int last=list.remove(list.size()-1);
-            int sec_last=list.remove(list.size()-1);
-            if(last>sec_last){
-                list.add(last-sec_last);
-            }
+        // List<Integer> list=new ArrayList<>();
+        // for(int k:stones){
+        //     list.add(k);
+        // }
+        // while(list.size()>1){
+        //     Collections.sort(list);
+        //     int last=list.remove(list.size()-1);
+        //     int sec_last=list.remove(list.size()-1);
+        //     if(last>sec_last){
+        //         list.add(last-sec_last);
+        //     }
 
+        // }
+        // if(list.size()==1){
+        //     return list.get(0);
+        // }
+        // return 0;
+        // // time complexity O(n*nlogn
+
+        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());  // FOR MAX HEAP
+        for(int k:stones){
+            pq.add(k);
         }
-        if(list.size()==1){
-            return list.get(0);
+        while(pq.size()>1){
+            int stone1=pq.poll();
+            int stone2=pq.poll();
+            if(stone1==stone2){
+                continue;
+            }
+            pq.add(stone1-stone2);
         }
+        if(pq.size()==1){
+            return pq.peek();
+        }
+        
         return 0;
     }
 }
