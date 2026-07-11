8e2ac555834cd04f7895245495f08118e1d76a04
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-11 18:37:56 +0530

    Time: 49 ms (69.51%), Space: 51.5 MB (71.75%) - LeetHub

--- 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
+++ 1942-the-number-of-the-smallest-unoccupied-chair/1942-the-number-of-the-smallest-unoccupied-chair.java
@@ -1,22 +1,58 @@
-class Solution {
-    public int smallestChair(int[][] times, int targetFriend) {
-        int targetFriendArrivalTime=times[targetFriend][0];
+// class Solution {
+//     public int smallestChair(int[][] times, int targetFriend) {
+//         int targetFriendArrivalTime=times[targetFriend][0];
 
-        Arrays.sort(times,(a,b)->a[0]-b[0]);
-        int[] chairs = new int[times.length];
+//         Arrays.sort(times,(a,b)->a[0]-b[0]);
+//         int[] chairs = new int[times.length];
+//         for(int i=0;i<times.length;i++){
+//             for(int j = 0;j<chairs.length;j++){
+//                 if(times[i][0]>=chairs[j]){
+//                     if(targetFriendArrivalTime==times[i][0]){
+//                         return j;
+//                     }
+//                     chairs[j]=times[i][1];
+//                     break;
+//                 }
+//             }
+//         }
+
+//        return -1; 
+//     }
+// }
+
+class Solution{ 
+    public int smallestChair(int[][] times,int targetFriend){
+        int targetFriendArrivalTime=times[targetFriend][0];
+        Arrays.sort(times,(a,b)-> a[0]-b[0]);
+
+        PriorityQueue<Integer> availableChairs = new  PriorityQueue<>();
+        PriorityQueue<int[]> occupiedChairs=new PriorityQueue<>((a,b)->a[0]-b[0]);
+
         for(int i=0;i<times.length;i++){
-            for(int j = 0;j<chairs.length;j++){
-                if(times[i][0]>=chairs[j]){
-                    if(targetFriendArrivalTime==times[i][0]){
-                        return j;
-                    }
-                    chairs[j]=times[i][1];
+            availableChairs.add(i);
+        }
+        for(int i=0;i<times.length;i++){
+            int aT=times[i][0];
+
+            while(!occupiedChairs.isEmpty()){
+                if(aT>=occupiedChairs.peek()[0]){
+                   int[] chair = occupiedChairs.poll();
+                   availableChairs.add(chair[1]);  
+                } else{
                     break;
                 }
             }
+           int c=availableChairs.poll();
+           if(targetFriendArrivalTime==aT){
+            return c;
+           }
+           occupiedChairs.add(new int[]{
+            times[i][1],c
+           });
         }
+        return -1;
 
-       return -1; 
+
     }
 }
 
