33f943dae9254879672635bc747a9bcdf7a97549
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-12 11:51:22 +0530

    Time: 33 ms (22.79%), Space: 49.2 MB (95.75%) - LeetHub

--- 0436-find-right-interval/0436-find-right-interval.java
+++ 0436-find-right-interval/0436-find-right-interval.java
@@ -1,24 +1,52 @@
 class Solution {
     public int[] findRightInterval(int[][] intervals) {
-        int len = intervals.length;
-        int[] res = new int [len];
-        for(int i=0;i<len;i++){
-            int minStartIdx=-1;
-            for(int j=0;j<len;j++){
-                if(intervals[j][0]>=intervals[i][1]){
-                    if(minStartIdx==-1){
-                        minStartIdx=j;
-                    }
-                    if(intervals[j][0]<intervals[minStartIdx][0]){
-                        minStartIdx=j;
-                    }
-                }
-            }
-            res[i]=minStartIdx;
-        }
-      return res;
+    //     int len = intervals.length;
+    //     int[] res = new int [len];
+    //     for(int i=0;i<len;i++){
+    //         int minStartIdx=-1;
+    //         for(int j=0;j<len;j++){
+    //             if(intervals[j][0]>=intervals[i][1]){
+    //                 if(minStartIdx==-1){
+    //                     minStartIdx=j;
+    //                 }
+    //                 if(intervals[j][0]<intervals[minStartIdx][0]){
+    //                     minStartIdx=j;
+    //                 }
+    //             }
+    //         }
+    //         res[i]=minStartIdx;
+    //     }
+    //   return res;
+
+       PriorityQueue<int[]> minStart=new PriorityQueue<>((a,b)->a[0]-b[0]);
+         PriorityQueue<int[]> minEnd=new PriorityQueue<>((a,b)->a[0]-b[0]);
         
-    }
+        int len=intervals.length;
+        int[] res=new int[len];
+        for(int i=0;i<len;i++){
+            res[i]=-1;
+            minStart.add(new int[]{
+                intervals[i][0],i
+            });
+            minEnd.add(new int[]{
+                intervals[i][1],i
+            });
+        }
+        while(!minStart.isEmpty()&&!minEnd.isEmpty()){
+         int[] startEle=minStart.peek();
+         int start=startEle[0];
+         int startIdx=startEle[1];
+
+         if(start>=minEnd.peek()[0]){
+            res[minEnd.peek()[1]]=startIdx;
+            minEnd.poll();
+         } else {
+            minStart.poll();
+         }
+        
+        }
+        return res;
+    }   
 }
 
 // Synced seamlessly with LeetHub Pro
