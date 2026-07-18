bf7042b6760d28c880558e062b931390a03f4be5
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-18 14:57:54 +0530

    Time: 0 ms (100%), Space: 48.3 MB (50.45%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+    int n=nums.length;
+    int lo=0,hi=n-1 ;
+    while(lo<=hi){
+        int mid=(lo+hi)/2;
+        if(nums[mid]==target) return mid;
+        else if(nums[mid]<target) lo=mid+1;
+        else hi=mid-1;
+    }
+    return -1;
+
+    }   
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
