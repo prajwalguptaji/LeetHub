27d88849613e365a25e3075956cd4f16352c1873
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-19 11:05:20 +0530

    Time: 24 ms (93.41%), Space: 52.1 MB (20.93%) - LeetHub

--- 0528-random-pick-with-weight/0528-random-pick-with-weight.java
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.java
@@ -1,35 +1,67 @@
-class Solution {
-    int[] pSums;
+// class Solution {
+//     int[] pSums;
 
-    public Solution(int[] w) {
-        pSums=new int[w.length];
-        // 4 10 5
-        // 4 14 19
-        pSums[0]=w[0];
-        for(int i=1;i<w.length;i++){
-            pSums[i]=pSums[i-1]+w[i];
-        }
-    }
+//     public Solution(int[] w) {
+//         pSums=new int[w.length];
+//         // 4 10 5
+//         // 4 14 19
+//         pSums[0]=w[0];
+//         for(int i=1;i<w.length;i++){
+//             pSums[i]=pSums[i-1]+w[i];
+//         }
+//     }
     
-    public int pickIndex() {
-        double rand=Math.random();
-        //0-0.99999
-        double target=rand*pSums[pSums.length-1];
-        for(int i=0;i<pSums.length;i++){
-            if(target<=pSums[i]){
-                return i;
-            }
-        }
-        return 0;
+//     public int pickIndex() {
+//         double rand=Math.random();
+//         //0-0.99999
+//         double target=rand*pSums[pSums.length-1];
+//         for(int i=0;i<pSums.length;i++){
+//             if(target<=pSums[i]){
+//                 return i;
+//             }
+//         }
+//         return 0;
 
-    }
-}
+//     }
+// }
 
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(w);
  * int param_1 = obj.pickIndex();
  */
+
+ class Solution {
+    int[] pSums;
+
+    public Solution(int[] w) {
+        pSums = new int[w.length];
+
+        pSums[0] = w[0];
+        for (int i = 1; i < w.length; i++) {
+            pSums[i] = pSums[i - 1] + w[i];
+        }
+    }
+
+    public int pickIndex() {
+        double target = Math.random() * pSums[pSums.length - 1];
+
+        int low = 0;
+        int high = pSums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (target < pSums[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
