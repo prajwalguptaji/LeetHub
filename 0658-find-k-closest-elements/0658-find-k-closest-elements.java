34f2b2226925e181e6ca3e11a4ab94bf0ca0ce87
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-19 13:25:23 +0530

    Time: 4 ms (96.87%), Space: 47.9 MB (35.53%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,32 +1,54 @@
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-       //1 2 3 4 5
-       //k=4
-       //x=3
-       //3,2,4,1
-       //1,2,3,4
-       //custom sorting fuction on integers
-       List<Integer> list=new ArrayList<>();
-       for(int ele:arr){
-           list.add(ele);
-       }
-       //sort absed on absolute diffrence from x
-       Collections.sort(list,(a,b)->{
-        int absAFromX=Math.abs(a-x);
-        int absBFromX=Math.abs(b-x);
-        if(absAFromX==absBFromX){
-           return a-b;
-        }
-        return absAFromX-absBFromX;
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//        //1 2 3 4 5
+//        //k=4
+//        //x=3
+//        //3,2,4,1
+//        //1,2,3,4
+//        //custom sorting fuction on integers
+//        List<Integer> list=new ArrayList<>();
+//        for(int ele:arr){
+//            list.add(ele);
+//        }
+//        //sort absed on absolute diffrence from x
+//        Collections.sort(list,(a,b)->{
+//         int absAFromX=Math.abs(a-x);
+//         int absBFromX=Math.abs(b-x);
+//         if(absAFromX==absBFromX){
+//            return a-b;
+//         }
+//         return absAFromX-absBFromX;
 
-       });
-       List<Integer> res=new ArrayList<>();
-       for(int i=0;i<k;i++){
-        res.add(list.get(i));
-       }
-       Collections.sort(res);
-       return res;
-    }
+//        });
+//        List<Integer> res=new ArrayList<>();
+//        for(int i=0;i<k;i++){
+//         res.add(list.get(i));
+//        }
+//        Collections.sort(res);
+//        return res;
+//     }
+// }
+
+class Solution {
+     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int low=0;
+      int  high=arr.length-k;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                low=mid+1;
+            }
+            else{
+                high=mid;
+            }
+        }
+        //low nearest element pr phuch chuka h
+        List<Integer> ans=new ArrayList<>();
+        for(int i=low;i<low+k;i++){
+            ans.add(arr[i]);
+        }
+        return ans;
+     }
 }
 
 // Synced seamlessly with LeetHub Pro
