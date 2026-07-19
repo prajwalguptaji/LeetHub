a2d251b673be3f424ad95dc8d43425e398ef947a
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-19 08:13:46 +0530

    Time: 0 ms (100%), Space: 53 MB (21.23%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,31 +1,50 @@
+// class Solution {
+//     public int singleNonDuplicate(int[] nums) {
+//     int low=0;
+//     int high=nums.length-1;
+//     while (low<high){
+//         int mid=low+(high-low)/2;
+//         int leftElements=mid-low+1,
+//             rightElements=high-mid+1;
+//        if(nums[mid]==nums[mid+1]){
+//           if(rightElements%2==1){//single present in right side element
+//             low=mid+2;
+//           }else{
+//             high=mid-1;
+//           }
+//        }else if(nums[mid]==nums[mid-1]) {
+//            if(leftElements%2==1){
+//               high=mid-2;
+//            }
+//            else{
+//               low=mid+1;
+//            }
+//        }else{
+//           return nums[mid];
+//        }
+//     } 
+//     return nums[low];
+//     }
+// }
+
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-    int low=0;
-    int high=nums.length-1;
-    while (low<high){
-        int mid=low+(high-low)/2;
-        int leftElements=mid-low+1,
-            rightElements=high-mid+1;
-       if(nums[mid]==nums[mid+1]){
-          if(rightElements%2==1){
-            low=mid+2;
-          }else{
-            high=mid-1;
-          }
-       }else if(nums[mid]==nums[mid-1]) {
-           if(leftElements%2==1){
-              high=mid-2;
-           }
-           else{
-              low=mid+1;
-           }
-       }else{
-          return nums[mid];
-       }
-    } 
-    return nums[low];
-    }
-}
+         public int singleNonDuplicate(int[] nums) {
+            int low=0,
+            high=nums.length-1;
+            while(low<high){
+                int mid=low+(high-low)/2;
+                if(mid%2==1){
+                    mid=mid-1;
+                }
+                if(nums[mid]!=nums[mid+1]){
+                    high = mid;
+                } else{
+                    low=mid+2;
+                }
+            }
+            return nums[low];
+        }
+}    
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
