dde62b80e0bbb6631b07918f854005f3c09f22aa
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-22 07:56:10 +0530

    Time: 1 ms (99.98%), Space: 71.4 MB (47.52%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+      // 0,1,2,3,  
+     // [3,4,-1,1]
+     int i=0,correctIndex=0;
+    
+     while(i<nums.length){
+         correctIndex=nums[i]-1;
+        if(correctIndex<0|| correctIndex>=nums.length){
+            i++;
+        }
+        else if(nums[i]!=nums[correctIndex]){
+            int temp=nums[i];
+            nums[i]=nums[correctIndex];
+            nums[correctIndex]=temp;
+        } else{
+            i++;
+        }
+
+     }  
+     //[-1,2,3,4]
+     for(i=0;i<nums.length;i++){
+       if( nums[i]!=i+1){
+        return i+1;
+       }
+     }
+     return i+1;
+     
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
