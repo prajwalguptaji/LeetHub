480e85861bcb0ad00300b88b514e3a120f474506
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-24 11:58:11 +0530

    Time: 7 ms (46.21%), Space: 41.9 MB (70.29%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,18 +1,27 @@
+// class Solution {
+//     public int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+//         int prev2=0;
+//         int prev1=1;
+//         for(int i=2;i<=n;i++){
+//            int  prev=prev1+prev2;
+//             prev2=prev1;
+//             prev1=prev;
+//         }
+//         return prev1;
+        
+//     }
+// }
+
 class Solution {
-    public int fib(int n) {
-        if(n<=1){
+    public static int fib(int n) {
+        if(n<2){
             return n;
         }
-        int prev2=0;
-        int prev1=1;
-        for(int i=2;i<=n;i++){
-           int  prev=prev1+prev2;
-            prev2=prev1;
-            prev1=prev;
-        }
-        return prev1;
-        
-    }
+        return fib(n-1)+fib(n-2);
+     }
 }
 
 // Synced seamlessly with LeetHub Pro
