7abfc07a16ea4b3ca30910745af3449e212fb777
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-29 00:34:28 +0530

    Added solution - Time: O(n * W), Space: O(n * W)

---
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    public int knapsack(int W, int val[], int wt[]) {
+
+        int n = wt.length;
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+
+                if (wt[i - 1] <= w) {
+                    dp[i][w] = Math.max(
+                            val[i - 1] + dp[i - 1][w - wt[i - 1]],
+                            dp[i - 1][w]);
+                } else {
+                    dp[i][w] = dp[i - 1][w];
+                }
+            }
+        }
+
+        return dp[n][W];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
