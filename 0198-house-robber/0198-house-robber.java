df2ff4eddb7993651e9a4dcc9ac8e5173b144b92
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-30 13:59:33 +0530

    Time: 0 ms (100%), Space: 42.8 MB (37.87%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,34 @@
-class Solution {
-    public int amount (int[] nums,int i,int[]dp){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take = nums[i]+amount(nums,i+2,dp);
-        int skip=amount(nums,i+1,dp);
-        return dp[i] = Math.max(take,skip);
-    }
-    public int rob(int[] nums) {
-        int[] dp=new int[nums.length];
-        Arrays.fill(dp,-1);
-       return amount(nums,0,dp); 
-    }
+// class Solution {
+//     // TC O(n)
+//     public int amount (int[] nums,int i,int[]dp){
+//         if(i>=nums.length) return 0;
+//         if(dp[i]!=-1) return dp[i];
+//         int take = nums[i]+amount(nums,i+2,dp);
+//         int skip=amount(nums,i+1,dp);
+//         return dp[i] = Math.max(take,skip);
+//     }
+//     public int rob(int[] nums) {
+//         int[] dp=new int[nums.length];
+//         Arrays.fill(dp,-1);
+//        return amount(nums,0,dp); 
+//     }
+// }
+
+// TABULATION
+class Solution{
+
+     public int rob(int[] nums) {
+         int n=nums.length;
+         if(n==1) return nums[0];
+         int[] dp=new int[n];
+         dp[0] =nums[0];
+         dp[1] =Math.max(nums[0],nums[1]);
+         for(int i=2;i<n;i++){
+            dp[i]=Math.max((nums[i]+dp[i-2]),dp[i-1]);
+        }
+        return dp[n-1];
+
+  }
 }
 
 // Synced seamlessly with LeetHub Pro
