4d508aa83af8aab7754d43e0157b351150c65bd2
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-30 11:36:28 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.87%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -25,25 +25,38 @@
 //      }
 // }
 
-// DP
-class Solution {
-    static int[]  dp;
-    public int fibo(int n,int[] dp) { 
-        if(n<2){
-           dp[n]=n;
-           return n;
+// DP memoization (top down DP)
+// class Solution {
+//     public int fibo(int n,int[] dp) { 
+//         if(n<2){
+//            dp[n]=n;
+//            return n;
             
-        }
-        if(dp[n]!=0)
-            return dp[n];
-        else{
-            dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
-            return dp[n];
-        }
-    }
+//         }
+//         if(dp[n]!=0)
+//             return dp[n];
+//         else{
+//             dp[n]=fibo(n-1,dp)+fibo(n-2,dp);
+//             return dp[n];
+//         }
+//     }
+//     public int fib(int n){
+//         dp=new int[n+1];
+//         return fibo(n,dp);
+//     }
+// }
+
+//DP tabulation method
+class Solution{
     public int fib(int n){
-        dp=new int[n+1];
-        return fibo(n,dp);
+        if(n<=1) return n;
+        int[] dp= new int[n+1];
+        dp[0]=0;dp[1]=1;
+        for(int i=2;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+            //0 1 1 2 3 5 8 13...
+        }
+        return dp[n];
     }
 }
 
