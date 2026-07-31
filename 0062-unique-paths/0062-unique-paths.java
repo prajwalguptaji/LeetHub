02535fe38ddb8cc8234304e5b6f3a1df44ccda32
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-07-31 09:36:33 +0530

    Time: 0 ms (100%), Space: 41.6 MB (98.18%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,22 +1,41 @@
+// class Solution {
+//     public int uniquePaths(int m, int n) {
+//        int[][] dp=new int[m][n];
+//        for(int i=0;i<m;i++){
+//         for(int j=0;j<n;j++){
+//             dp[i][j]=-1;
+//         }
+//        } 
+//        return paths(0,0,m,n,dp);
+//     }
+//     public int paths(int row,int col,int m,int n,int[][]dp){
+//         if(row>=m || col>=n) return 0;
+//         if(row==m-1 && col==n-1) return 1;
+//         if(dp[row][col]!=-1) return dp[row][col];
+//         int rightWays=paths (row,col+1,m,n,dp);
+//         int downWays=paths(row+1,col,m,n,dp);
+//         return dp[row][col]=rightWays+downWays;
+//     }
+// }
 class Solution {
     public int uniquePaths(int m, int n) {
-       int[][] dp=new int[m][n];
-       for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            dp[i][j]=-1;
-        }
-       } 
-       return paths(0,0,m,n,dp);
+        int[][] dp=new int[m][n];
+         for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0){
+                   dp[i][j]= 1;
+                }else{
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];  
+                }  
+            }
+            
+         }
+        return dp[m-1][n-1];        
     }
-    public int paths(int row,int col,int m,int n,int[][]dp){
-        if(row>=m || col>=n) return 0;
-        if(row==m-1 && col==n-1) return 1;
-        if(dp[row][col]!=-1) return dp[row][col];
-        int rightWays=paths (row,col+1,m,n,dp);
-        int downWays=paths(row+1,col,m,n,dp);
-        return dp[row][col]=rightWays+downWays;
-    }
+    
 }
+
+
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
