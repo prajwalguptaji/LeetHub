bbec05a59e8f1338d5cfb0371e3abdb233ba9481
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-08-07 13:57:02 +0530

    Time: 4 ms (7.4%), Space: 45.2 MB (7.16%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,31 +1,47 @@
+// class Solution {
+
+//     public List<String> generateParenthesis(int n) {
+
+//         List<String> ans = new ArrayList<>();
+
+//         helper("", 0, 0, n, ans);
+
+//         return ans;
+//     }
+
+//     void helper(String s, int open, int close, int n,
+//                 List<String> ans) {
+
+//         if (s.length() == 2 * n) {
+//             ans.add(s);
+//             return;
+//         }
+
+//         // Add '('
+//         if (open < n) {
+//             helper(s + "(", open + 1, close, n, ans);
+//         }
+
+//         // Add ')'
+//         if (close < open) {
+//             helper(s + ")", open, close + 1, n, ans);
+//         }
+//     }
+// }
 class Solution {
-
-    public List<String> generateParenthesis(int n) {
-
-        List<String> ans = new ArrayList<>();
-
-        helper("", 0, 0, n, ans);
-
-        return ans;
-    }
-
-    void helper(String s, int open, int close, int n,
-                List<String> ans) {
-
-        if (s.length() == 2 * n) {
+    public static void generator(int n,int l,int r,String s,List<String> ans){
+        if(r==n){
             ans.add(s);
             return;
         }
-
-        // Add '('
-        if (open < n) {
-            helper(s + "(", open + 1, close, n, ans);
-        }
-
-        // Add ')'
-        if (close < open) {
-            helper(s + ")", open, close + 1, n, ans);
-        }
+        if(l<n) generator(n,l+1,r,s+"(",ans);
+        if(r<l) generator(n,l,r+1,s+")",ans); 
+    }
+    public List<String> generateParenthesis(int n) {
+        List<String> ans=new ArrayList<>();
+        generator(n,0,0,"",ans);
+        System.out.println(ans);
+        return ans;
     }
 }
 
