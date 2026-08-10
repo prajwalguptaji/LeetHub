d3902ef234a74c90d2f1b6eae1a87ec9557b74fd
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-08-10 07:39:37 +0530

    Time: 0 ms (100%), Space: 43 MB (23.46%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int amount(int[] nums, int i, int end, int[] dp) {
+        if (i > end)
+            return 0;
+        if (dp[i] != -1)
+            return dp[i];
+        int take = nums[i] + amount(nums, i + 2, end, dp);
+        int skip = amount(nums, i + 1, end, dp);
+        return dp[i] = Math.max(take, skip);
+    }
+  public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1)
+            return nums[0];
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        int case1 = amount(nums, 0, n - 2, dp1);
+        int case2 = amount(nums, 1, n - 1, dp2);
+        return Math.max(case1, case2);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
