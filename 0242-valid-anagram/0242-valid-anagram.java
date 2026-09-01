10c6d726e5f80083fdde361bbf12f2ceff01442b
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-09-01 14:27:27 +0530

    Time: 4 ms (83.52%), Space: 44.1 MB (89.74%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,10 +3,10 @@ class Solution {
         if(s.length()!=t.length()){
             return false;
         }
-        int[] count=new int[26];
+        int[] count=new int[256];
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
+            count[s.charAt(i)]++;
+            count[t.charAt(i)]--;
         }
         for(int i=0;i<count.length;i++){
             if(count[i]!=0){
