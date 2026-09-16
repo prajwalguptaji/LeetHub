a65dccdfccb799cdee1ef865ac83c803780a1b8f
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-09-16 09:47:58 +0530

    Time: 0 ms (100%), Space: 46.3 MB (83.26%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit=0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                profit += prices[i] - prices[i - 1];
+            }
+        }
+        return profit;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
