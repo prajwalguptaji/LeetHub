b7fc5460ff418baf402073db4249002b3a25d6ba
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-09-20 07:38:40 +0530

    Time: 25 ms (23.32%), Space: 47 MB (20.21%) - LeetHub

--- 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
+++ 3305-count-of-substrings-containing-every-vowel-and-k-consonants-i/3305-count-of-substrings-containing-every-vowel-and-k-consonants-i.java
@@ -1,36 +1,28 @@
 import java.util.*;
-
 class Solution {
     public int countOfSubstrings(String word, int k) {
         int n = word.length();
         int count = 0;
-
         for (int i = 0; i < n; i++) {
             Set<Character> vowels = new HashSet<>();
             int consonants = 0;
-
             for (int j = i; j < n; j++) {
                 char ch = word.charAt(j);
-
                 if (isVowel(ch)) {
                     vowels.add(ch);
                 } else {
                     consonants++;
                 }
-
                 if (consonants > k) {
                     break;
                 }
-
                 if (consonants == k && vowels.size() == 5) {
                     count++;
                 }
             }
         }
-
         return count;
     }
-
     private boolean isVowel(char ch) {
         return ch == 'a' || ch == 'e' || ch == 'i' ||
                ch == 'o' || ch == 'u';
