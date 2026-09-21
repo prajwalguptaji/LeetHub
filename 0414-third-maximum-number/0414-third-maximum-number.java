4ee6408e9996de91e77d3aea5ba156171dcee20b
prajwalguptaji <prajwalgupta.ishu@gmail.com>
2026-09-21 08:25:12 +0530

    Time: 3 ms (62.05%), Space: 44.8 MB (54.29%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -1,19 +1,49 @@
+// class Solution {
+//     public int thirdMax(int[] nums) {
+//         Arrays.sort(nums);
+//         int count = 1;
+//         int curr = nums[nums.length - 1];
+//         for (int i = nums.length - 2; i >= 0; i--) {
+//             if (curr == nums[i]) {
+//                 continue;
+//             }
+//             curr = nums[i];
+//             count++;
+//             if (count == 3) {
+//                 return curr;
+//             }
+//         }
+//         return nums[nums.length - 1];
+//     }
+// }
+
+//
 class Solution {
     public int thirdMax(int[] nums) {
-        Arrays.sort(nums);
-        int count = 1;
-        int curr = nums[nums.length - 1];
-        for (int i = nums.length - 2; i >= 0; i--) {
-            if (curr == nums[i]) {
-                continue;
-            }
-            curr = nums[i];
-            count++;
-            if (count == 3) {
-                return curr;
+        Long first = null;
+        Long second = null;
+        Long third = null;
+        for (int num : nums) {
+            long n = num;
+            // Skip duplicates
+            if (first != null && n == first) continue;
+            if (second != null && n == second) continue;
+            if (third != null && n == third) continue;
+            if (first == null || n > first) {
+                third = second;
+                second = first;
+                first = n;
+            } 
+            else if (second == null || n > second) {
+                third = second;
+                second = n;
+            } 
+            else if (third == null || n > third) {
+                third = n;
             }
         }
-        return nums[nums.length - 1];
+        // If 3 distinct numbers don't exist, return maximum
+        return third == null ? first.intValue() : third.intValue();
     }
 }
 
